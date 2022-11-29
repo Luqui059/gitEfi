ee6e06dc39ccd9300bb7d7878808e7e17cb36a33
J.Lucas <sumorevi@gmail.com>
2022-11-29 20:21:07 -0300

    Final, espero

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Para ingresar en la app dirigirse a http://localhost:8080
+y las rutas que hicimos son:
+http://localhost:8000/home
+http://localhost:8000/index
+http://localhost:8000/info
+http://localhost:8000/parametros
--- app/app.py
+++ app/app.py
@@ -1,9 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-@app.route('/')
-def hola():
-	return "POR FIN ANDA!!!!!!\n"
+@app.route("/")
+def index():
+    return 'Index'
+
+@app.route("/info")
+def info():
+    argumentos = request.args
+    if argumentos != {}:
+        return argumentos
+    return 'No hay argumentos'
+
+
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/parametros")
+def parametros():
+    lista_nombres = ['Juan', 'Pedro', 'Ana', 'Maria']
+    return render_template('parametros.html', nombres=lista_nombres)
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000)
