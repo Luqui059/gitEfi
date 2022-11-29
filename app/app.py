3911fc991bf62cdc62a3ef350dac56bd50ea5f63
J.Lucas <sumorevi@gmail.com>
2022-11-29 18:41:09 -0300

    INTENTO 90011

--- app/app.py
+++ app/app.py
@@ -1,11 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-	return "FUNCIONA!!! AL FIN\n"
+def anda():
+	return "POR FIN ANDA\n"
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
-
